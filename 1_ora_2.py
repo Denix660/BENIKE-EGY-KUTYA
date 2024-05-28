3116258c6e7beac99312cd3bd68bf2a7af21b26e
Szabolcs Joo <126967005+Denix660@users.noreply.github.com>
2024-05-28 11:55:11 +0200

    Add files via upload

---
+++ 1_ora_2.py
@@ -0,0 +1,21 @@
+nev = "Kovács Péter"
+magyar_tanarom_neve = "még nincs" #snake case (camel case)
+PI = 3.14 #állandó
+_1_ora = "magyar"
+printels = "hello nyomtasd ki ezt"
+print(printels)
+
+knev = "Peti"
+vnev = "Kiss"
+print(knev,vnev)
+
+eletkor = 15
+
+print("A keresztnevem",knev,"a vezeteknevem",vnev,)
+print("A keresztnevem "+knev+" a vezeteknevem "+vnev)
+
+print("A keresztnevem",knev,"a vezeteknevem",vnev,"eletkorom:",eletkor)
+print("A keresztnevem "+knev+" a vezeteknevem "+vnev+"eletkorom:" ,eletkor)
+
+print(f'A keresztevem:{knev} a vezeteknevem:{vnev} eletkorom:{eletkor}')
+
